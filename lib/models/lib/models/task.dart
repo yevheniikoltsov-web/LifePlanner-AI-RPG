f65722f1e56cdb3939d544ca87ea4af132d89e18
yevheniikoltsov-web <yevheniikoltsov@gmail.com>
2026-03-26 12:18:45 +0100

    Create task.dart

---
+++ lib/models/lib/models/task.dart
@@ -0,0 +1,17 @@
+class Task {
+  String title;
+  String statType;
+  TimeOfDay startTime;
+  TimeOfDay endTime;
+  bool isCompleted;
+  DateTime date; // НОВО: дата задачи
+
+  Task({
+    required this.title,
+    required this.statType,
+    required this.startTime,
+    required this.endTime,
+    this.isCompleted = false,
+    required this.date,
+  });
+}
